ab6d65a8d49cd7bbe10d0f09a232307e535414f2
snowjay7 <ogwu.justina@gmail.com>
2022-09-13 08:17:20 -0700

    My update 8-24_hours.c commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,24 +1,30 @@
 #include "main.h"
 
 /**
- * jack_bauer - print every minute
+ * jack_bauer - print every minute of the day of jack bauer
+ * starting from 00:00 to 23:59
+ *
  */
-
 void jack_bauer(void)
 {
-	int h;
-	int m;
+	int i, j;
 
-	for (h = 0, h <= 23, h++)
+	i = 0;
+
+	while (i < 24)
 	{
-		for (m = 0, m <= 59, m++)
+		j = 0;
+
+		while (j < 60)
 		{
-			_putchar((h / 10) + '0');
-			_putchar((h % 10) + '0');
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 			_putchar(':');
-			_putchar((m / 10) + '0');
-			_putchar((m % 10) + '0');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 			_putchar('\n');
+			j++;
 		}
+		i++;
 	}
 }
